667d5f15b091ba4d1dc976b5c860a82d5ab2a516
sarantic <150983455+sarantic@users.noreply.github.com>
2025-08-02 15:06:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ const scenes = [scene1, scene2, scene3]; //defined below
 function init() {
   d3.csv("data/median_average_wages.csv").then(function(data) {
     data.forEach(d => {
-      d.date = new Date(d.date);
       d.Year = +d.Year;
       d.Median = +d.Median;
       d.Average = +d.Average;
     });
 
-    // Save data for use in all scenes
     window.wageData = data;
-
-    // Start with the first scene
     scenes[sceneIndex]();
   });
 }
+
 
 function nextScene() {
   sceneIndex = (sceneIndex + 1) % scenes.length;
@@ -40,6 +37,46 @@ function scene1() {
       <p>Let's explore how this affects everyday workers, starting in 1973.</p>
     `);
 }
+function scene2() {
+  d3.select("#vis").html(""); // clear container
+
+  const svg = d3.select("#vis").append("svg")
+                .attr("width", 700)
+                .attr("height", 400);
+
+  const data = window.wageData;
+
+  const x = d3.scaleLinear().domain(d3.extent(data, d => d.Year)).range([50, 650]);
+  const y = d3.scaleLinear().domain([0, d3.max(data, d => d.Average)]).range([350, 50]);
+
+  const line = d3.line()
+                 .x(d => x(d.Year))
+                 .y(d => y(d.Average));
+  const xAxis = d3.axisBottom(x).tickFormat(d3.format("d"));
+  const yAxis = d3.axisLeft(y);
+
+  svg.append("g")
+   .attr("transform", "translate(0,350)")
+   .call(xAxis);
+
+  svg.append("g")
+   .attr("transform", "translate(50,0)")
+   .call(yAxis);
+
+
+  svg.append("path")
+     .datum(data)
+     .attr("fill", "none")
+     .attr("stroke", "orange")
+     .attr("stroke-width", 2)
+     .attr("d", line);
+
+  svg.append("text")
+     .attr("x", 50)
+     .attr("y", 30)
+     .text("Average Hourly Wages Over Time");
+}
+
 
 
 

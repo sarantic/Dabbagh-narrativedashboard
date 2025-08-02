b1cf04302eb3b9ba72c7e29c9554eb46e91b77b3
sarantic <150983455+sarantic@users.noreply.github.com>
2025-08-02 15:02:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let sceneIndex = 0;
 const scenes = [scene1, scene2, scene3]; //defined below
 
 function init() {
-  d3.csv("data/DailyDelhiClimateTest.csv").then(function(data) {
+  d3.csv("data/median_average_wages.csv").then(function(data) {
     data.forEach(d => {
       d.date = new Date(d.date);
-      d.mean_temperature = +d.mean_temperature;
-      d.humidity = +d.humidity;
-      d.wind_speed = +d.wind_speed;
+      d.Year = +d.Year;
+      d.Median = +d.Median;
+      d.Average = +d.Average;
     });
 
     // Save data for use in all scenes
-    window.climateData = data;
+    window.wageData = data;
 
     // Start with the first scene
     scenes[sceneIndex]();
@@ -29,32 +29,17 @@ function prevScene() {
 
 }
 function scene1() {
-  d3.select("#vis").html(""); // clear the container
-  const svg = d3.select("#vis").append("svg")
-                .attr("width", 600)
-                .attr("height", 400);
+  d3.select("#vis").html(""); // clear
 
-  const data = window.climateData;
-
-  // Temperature line chart
-  const x = d3.scaleTime().range([50, 550]).domain(d3.extent(data, d => d.date));
-  const y = d3.scaleLinear().range([350, 50]).domain(d3.extent(data, d => d.mean_temperature));
-
-  const line = d3.line()
-                 .x(d => x(d.date))
-                 .y(d => y(d.mean_temperature));
-
-  svg.append("path")
-     .datum(data)
-     .attr("fill", "none")
-     .attr("stroke", "steelblue")
-     .attr("stroke-width", 2)
-     .attr("d", line);
-
-  svg.append("text")
-     .attr("x", 50)
-     .attr("y", 30)
-     .text("Daily Mean Temperature in Delhi");
+  d3.select("#vis")
+    .append("div")
+    .attr("class", "intro-text")
+    .html(`
+      <h2>Wage Growth in the United States</h2>
+      <p>Over the past five decades, how have average and median wages changed?</p>
+      <p>Let's explore how this affects everyday workers, starting in 1973.</p>
+    `);
 }
+
 
 
